5e16e01ad4e0382ee2ad30d25e5ed7831043e7f3
ammar-rasheed@hotmail.com <ammar-rasheed@hotmail.com>
2023-07-22 18:59:14 +0300

    Initial commit

--- express.js
+++ express.js
@@ -1,8 +1,8 @@
+
 import threeCommasAPI from '3commas-api-node';
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
-import favicon from 'serve-favicon';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,6 +12,8 @@ const apiKey1 = '35c1fd4032924b3aad132a6d135294f8192816f541da44039dcea439f3e6cce
 const apiSecret1 = '25d7622eb5df82eace388295ab8872cdd6b6ea89dfb66bd31dde8ae26cd904c9f32511bdb166baa5b3ac8ddfb181d79e838636ec6baf929a454ad1e96d67e5ffa8d98091d9d51b044122c43638ea0a473688e48832d3a28ba81777bf965a23c5d37b1825';
 const apiKey2 = '4d92b7d77e504372aa71127afca240acf546cf35d70a4dc79d8cc97b85a397c2';
 const apiSecret2 = '43c7fe02916ed459d08b03ef3b45315168a2487e15eedbb34a6fc6a7cd37923c7cfc5d59f120daf2d351e9869bfcef2bc49e40828306f21c1179c229d799afde6862d16d255baca141d36ec3619e37963c66b2e72c3ca0555c3b040b4efee9dccd6d430a';
+
+
 
 const api1 = new threeCommasAPI({
   apiKey: apiKey1,
@@ -25,8 +27,11 @@ const api2 = new threeCommasAPI({
 
 const app = express();
 
+// Define the path to your favicon
 const faviconPath = path.join(__dirname, 'favicon.ico');
 
+// Remove the 'serve-favicon' middleware, as it is no longer a dependency
+// app.use(favicon(faviconPath));
 
 // Define a route for the root endpoint
 app.get('/', (req, res) => {
